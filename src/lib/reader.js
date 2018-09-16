359a47b8789f58761f45d1e4580581599111ebb4
Tom <tnorth93@gmail.com>
2018-09-16 15:16:28 -0700

    finished everything except readme

--- src/lib/reader.js
+++ src/lib/reader.js
@@ -3,14 +3,39 @@
 const fs = require('fs');
 
 const reader = module.exports = {};
+const firstPath = `${__dirname}/../data/one.txt`;
+const secondPath = `${__dirname}/../data/two.txt`;
+const thirdPath = `${__dirname}/../data/three.txt`;
 
-reader.readFiles = (paths, characters, callback) => {
-  return fs.readFile(files, (error, data) {
+const filePathArray = [firstPath, secondPath, thirdPath];
+
+reader.readFilesNCharactersAsync = (filePath, callback) => {
+  const mappedArray = [];
+  fs.readFile(filePathArray[0], (error, data) => {
     if (error) {
-      console.log('there was an issue reading the files');
-      console.log(error);
-      return error;
+      callback(error);
     }
-    return callback(data.toString, 'utf8', 0, characters);
+    mappedArray.push(data.toString('utf8'));
+
+    fs.readFile(filePathArray[1], (error2, data2) => {
+      if (error) {
+        callback(error2);
+      }
+      mappedArray.push(data2.toString('utf8'));
+      fs.readFile(filePathArray[2], (error3, data3) => {
+        if (error) {
+          callback(error3);
+        }
+        mappedArray.push(data3.toString('utf8'));
+        if (mappedArray[2]) {
+          callback(mappedArray);
+        }
+      });
+    });
   });
-}
+};
+
+// reader.readFilesNCharactersAsync(filePathArray[2], (data) => {
+//   console.log(data.toString('utf8'));
+//   return data.toString('utf8');
+// });
